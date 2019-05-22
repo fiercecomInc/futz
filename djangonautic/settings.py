09d5ca3f54c5535816325fc3206554d569a5b5ca
fiercecomInc <ojtFiercecom@gmail.com>
2019-05-22 12:09:32 +0800

    first commit

--- djangonautic/settings.py
+++ djangonautic/settings.py
@@ -62,7 +62,7 @@ ROOT_URLCONF = 'djangonautic.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [os.path.join(os.path.dirname(BASE_DIR),'templates')],
+        'DIRS': [(os.path.dirname(BASE_DIR),'templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
