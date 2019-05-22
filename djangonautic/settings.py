c7ca86a45702d80e94f03796cc263a6137917be1
fiercecomInc <ojtFiercecom@gmail.com>
2019-05-22 10:02:26 +0800

    first commit

--- djangonautic/settings.py
+++ djangonautic/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'ttnsqx@rod3n6tnevc57eaekwy@yydnqqxv8hd14!1u)+s4pvd'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['127.0.0.1','pythonanywhere']
+ALLOWED_HOSTS = ['127.0.0.1','adscititious.pythonanywhere.com']
 
 # Application definition
 
